bb63fecd7f49c3abfd35188b1dc3dc386bf9fb07
Diego Baes <diegobaesv@gmail.com>
2021-03-07 21:32:11 -0500

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 export const sequelize = new Sequelize(
     'bxzj94cwirjuk7awm0r2', //db_name
     'unvma5zwfprq4pulnqrr', //username
-    '5d66b5f134a750b2b5495cdafb6e3742e7064fad09e6314e2adc6ee09be3a22a', //password
+    'gScchCjZexAmhtpUEszi', //password
     {
         host: 'bxzj94cwirjuk7awm0r2-postgresql.services.clever-cloud.com',
         dialect: 'postgres', 
