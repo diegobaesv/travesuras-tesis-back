2caa38e75b9888b343ca2747536ebeca63235a04
Diego Baes <diegobaesv@gmail.com>
2021-03-07 21:27:36 -0500

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize');
 
 // Option 1: Passing a connection URI
 export const sequelize = new Sequelize(
-    'delas2kqd29t8m', //db_name
-    'xmunaicpojbfuo', //username
+    'bxzj94cwirjuk7awm0r2', //db_name
+    'unvma5zwfprq4pulnqrr', //username
     '5d66b5f134a750b2b5495cdafb6e3742e7064fad09e6314e2adc6ee09be3a22a', //password
     {
-        host: 'ec2-54-159-175-113.compute-1.amazonaws.com',
+        host: 'bxzj94cwirjuk7awm0r2-postgresql.services.clever-cloud.com',
         dialect: 'postgres', 
         port: '5432',
         pool: {
